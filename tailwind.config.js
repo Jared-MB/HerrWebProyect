5a075cdcbfb549e9f264d891c186a222741caac1
JaredM <amunozbaez669@gmail.com>
2022-11-29 19:56:34 -0600

    champions update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./proyecto.html"],
+  content: ["./src/**/*.{html,js}", "./*.html"],
   theme: {
     extend: {
       fontSize: {
